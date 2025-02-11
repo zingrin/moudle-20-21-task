b3e9366c0c5a82f67b9e73f72c42a20bb71458c9
zingrin <jerinloncheu@gmail.com>
2025-02-11 22:59:39 +0600

    problems sloved

---
+++ problems.js
@@ -0,0 +1,30 @@
+// 1 -5 moddhay 3 and 5 bivhajjo songkha ber koro
+
+
+
+// let temp = []
+// for(let i =1; i < 51;i++){
+//     if(i % 3==0 || i % 5==0){
+//         temp.push(i)
+//     }
+// }
+// console.log(temp)
+
+let temp = []
+for(let i =1; i < 51;i++){
+    if(i % 3==0 && i % 5==0){
+        temp.push(i)
+    }
+}
+// console.log(temp)
+var friends = ["rahim","karim","abdul","sadad","heroAlom"];
+// oporer kon naam ta sob theke boro ber koro.
+
+let bigFriends = friends[0];
+for (let i =0;i <friends.length; i++){
+    const element = friends[i];
+    if(element.length > bigFriends.length){
+        bigFriends = element;
+    }
+}
+console.log(bigFriends)
