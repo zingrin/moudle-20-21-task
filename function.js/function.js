a333c787a14ebdfa414fe817d5ce0c5adbec0801
zingrin <jerinloncheu@gmail.com>
2025-02-13 11:15:40 +0600

    function system

---
+++ function.js/function.js
@@ -0,0 +1,18 @@
+const price = 45;
+// function declare
+function fanOffKor(){
+console.log('bosa theke uthay dara');
+console.log('walk towards the switch');
+console.log('click the switch to off the fan');
+}
+// call the function
+fanOffKor();
+
+function brushTeeth() {
+    console.log("brush daily kora uccchit")
+    console.log(" daily tmk 2 bar brush korte hbe")
+    console.log("khawar aga hath clean kora uchit")
+    console.log("hey you")
+    
+}
+brushTeeth();
---
+++ function.js/square.js
@@ -0,0 +1,23 @@
+// 3...>3*3=9
+// 4...>4*4=16
+
+function square(number){
+console.log('value of the number parameter',number);
+const square = number * number;
+console.log('square of the given number is:', square)
+}
+// square(9)
+
+
+function add(num1, num2){
+const sum = num1 + num2;
+console.log(sum);
+}
+add(5, 71);
+add(65,31);
+function addAll(a,b,c,d,e){
+    const total = a + b + c + d + e;
+    console.log(total);
+
+}
+addAll(3, 4, 5, 6, 7);
