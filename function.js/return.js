01916152c624d96fc39e9748f5c62ed648cacb22
zingrin <jerinloncheu@gmail.com>
2025-02-13 15:45:06 +0600

    conditional return

--- function.js/return.js
+++ function.js/return.js
@@ -9,5 +9,14 @@ return result ;
 
 const bigNumber = tenTimes(99);
 tenTimes(99);
-const output = tenTimes(99);
-console.log('output', output);
+// const output = tenTimes(99);
+// console.log('output', output);
+  
+
+function itemsMenu(cards){
+const result = cards / 2;
+return result;
+}
+itemsMenu(100);
+const output = itemsMenu(100);
+console.log('output',output)
