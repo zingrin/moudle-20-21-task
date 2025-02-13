4c99fac256897447e7533de71b18da0abda14eb6
zingrin <jerinloncheu@gmail.com>
2025-02-13 14:36:11 +0600

    return

---
+++ function.js/arguments.js
@@ -0,0 +1,14 @@
+function doubleIt(number){
+    const double = number * 2;
+    console.log(number,double)
+
+}
+// doubleIt(77);
+
+function difference(num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2,'difference is',diff)
+
+} fatherAge = 40;
+myAge = 10;
+difference(fatherAge,myAge)
---
+++ function.js/return.js
@@ -0,0 +1,13 @@
+function tenTimes(number){
+const result = number * 10;
+return result ;
+}
+
+// tenTimes(5);
+// const output = tenTimes(5);
+// console.log('output', output)
+
+const bigNumber = tenTimes(99);
+tenTimes(99);
+const output = tenTimes(99);
+console.log('output', output);
